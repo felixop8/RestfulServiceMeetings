9e3cefcdf7128a51e56b22cab9ca42d4e26c6b75
felixop8 <felixop8@gmail.com>
2017-11-25 14:29:23 -0800

    create Restful routes

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,21 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::group(['prefix' => 'api/v1'], function() {
 
-Route::get('/', function () {
-    return view('welcome');
+    Route::resource('meeting', 'MeetingController', [
+        'except' => ['edit', 'create']
+    ]);
+
+    Route::resource('meeting/registration', 'RegistrationController', [
+        'only' => ['store', 'destroy']
+    ]);
+
+    Route::post('user', [
+        'uses' => 'AuthController@store'
+    ]);
+
+    Route::post('user/sigin', [
+        'uses' => 'AuthController@sigin'
+    ]);
+
 });
